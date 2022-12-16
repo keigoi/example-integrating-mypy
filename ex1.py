751d627227b6640369fe717fef7ae45f8f92c27c
Keigo Imai <keigo.imai@gmail.com>
2022-12-16 13:20:00 +0900

    wip

--- ex1.py
+++ ex1.py
@@ -1,2 +1,43 @@
-def f(x:int):
-    return x + 1
+from typing import TypeVar, Generic
+
+# def f(x:int):
+#     return x + 1
+
+T = TypeVar("T")
+U = TypeVar("U")
+
+class Roled1(Generic[T]):
+    pass
+
+class Roled2(Generic[T,U]):
+    pass
+
+class rstr(str, Roled1[T]):
+    pass
+
+class channel(Roled2[T,U]):
+    pass
+
+class Role:
+    pass
+
+class A(Role):
+    def __matmul__(self, x): # @ を使えるようにする
+        pass
+    pass
+
+def at(x:T, role:Role) -> T:
+    return x
+
+def g(x:str):
+    # A() @ ""
+    # x @ A()
+    # A() @ 123
+    return x
+
+# x = 1
+# if x:
+#     f()
+# else:
+#     g()
+
--- mypytest.py
+++ mypytest.py
@@ -7,16 +7,20 @@ import mypy.nodes
 import mypy.checker
 from mypy.plugin import CheckerPluginInterface
 import mypy.type_visitor
+import mypy.types
 
-result : mypy.build.BuildResult = mypycustom.main(["--show-traceback", "--verbose", "ex1.py"])
+result: mypy.build.BuildResult = mypycustom.main(["--show-traceback",
+                                                  #  "--verbose",
+                                                   "--config-file", "atmark_plugin_mypy.ini",
+                                                  "ex1.py"])
+
 
 class MyVisitor(mypy.visitor.NodeVisitor[None]):
-    type_checker : mypy.checker.TypeChecker
+    type_checker: mypy.checker.TypeChecker
 
-    def __init__(self, checker:mypy.checker.TypeChecker):
-        print("init success")
+    def __init__(self, checker: mypy.checker.TypeChecker):
         self.type_checker = checker
-    
+
     def visit_func_def(self, defn: mypy.nodes.FuncDef) -> None:
         defn.body.accept(self)
 
@@ -25,13 +29,27 @@ class MyVisitor(mypy.visitor.NodeVisitor[None]):
         for s in b.body:
             s.accept(self)
 
-    def visit_return_stmt(self, s :mypy.nodes.ReturnStmt) -> None:
+    def visit_return_stmt(self, s: mypy.nodes.ReturnStmt) -> None:
         print("visit return stmt")
         print(s.expr.accept(self.type_checker.expr_checker))
+        t: mypy.types.Type = s.expr.accept(
+            self.type_checker.expr_checker)  # たとえば t = rstr[A] なら
+        print("printing class of:" + str(t))
+        print(type(t))  # mypy.types.Instance
+
+        if isinstance(t, mypy.types.Instance):
+            print("printing class of t.args:" + str(t.args))
+            print(type(t.args))
         pass
+
+
+if result is None:
+    sys.exit(1)
 
 src = result.graph["ex1"]
 
 for d in src.tree.defs:
     v = MyVisitor(src.type_checker())
     d.accept(v)
+    if isinstance(d, mypy.types.Instance):
+        pass
